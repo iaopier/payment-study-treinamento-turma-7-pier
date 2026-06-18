017e1bbd1223462097cacd98bd112c45184355b5
Piercarlo Fermino Soares <pierhett@gmail.com>
2026-06-18 09:11:20 -0300

    feat(routes): [KAN-2] Auditoria e refatoração do arquivo de rotas principal para g

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,1 @@
-import { FastifyInstance } from 'fastify'
-import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
-import { userRoutes } from './user.routes.ts'
-
-
-export function appRoutes(app: FastifyInstance) {
-    app.setSerializerCompiler(serializerCompiler);
-    app.setValidatorCompiler(validatorCompiler);
-    app.register(userRoutes, { prefix: '/users' });
-}
+import { FastifyInstance } from 'fastify'; import { userRoutes } from './user.routes'; export async function registerRoutes(app: FastifyInstance): Promise<void> { await app.register(userRoutes, { prefix: '/users' }); }
