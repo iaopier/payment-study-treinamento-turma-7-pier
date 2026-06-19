af3edf6df386daa3152e76c403816810149ab570
Piercarlo Fermino Soares <pierhett@gmail.com>
2026-06-19 10:11:02 -0300

    feat(services): [KAN-5] Implementação da interface e serviço AuthResolver utilizando

---
+++ src/services/AuthResolver.ts
@@ -0,0 +1,1 @@
+import bcrypt from 'bcrypt'; export interface AuthResolver { validate(password: string, hash: string): Promise<boolean>; } export class AuthResolverImpl implements AuthResolver { async validate(password: string, hash: string): Promise<boolean> { return await bcrypt.compare(password, hash); } }
