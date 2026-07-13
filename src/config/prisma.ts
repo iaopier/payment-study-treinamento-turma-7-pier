85f0cb92c3f9d4ff2209e0dbb5fe71c1c0d4bcce
Piercarlo Fermino Soares <pierhett@gmail.com>
2026-07-13 15:16:38 -0300

    feat(config): [KAN-6] Exportação da instância do Prisma Client.

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,3 +1,1 @@
-fix: corrige src/config/prisma.ts (QA human review #1)
-
-Exportação da instância do Prisma Client.
+import { PrismaClient } from '@prisma/client'; export const prisma = new PrismaClient();
