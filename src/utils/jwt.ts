c27fb9f43e796b80c15f36e52baa1a08475f4d33
Piercarlo Fermino Soares <pierhett@gmail.com>
2026-07-13 13:40:26 -0300

    feat(utils): [KAN-6] Implementação de utilitários para geração e verificação de t

---
+++ src/utils/jwt.ts
@@ -0,0 +1,1 @@
+import jwt from 'jsonwebtoken'; const SECRET = process.env.JWT_SECRET || 'supersecret'; export const generateToken = (payload: object) => jwt.sign(payload, SECRET, { expiresIn: '1h' }); export const verifyToken = (token: string) => jwt.verify(token, SECRET);
