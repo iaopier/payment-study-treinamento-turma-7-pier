cb2cea79b390d24b112294b2f06c61cabb0b693f
Piercarlo Fermino Soares <pierhett@gmail.com>
2026-07-14 09:28:05 -0300

    fix: corrige src/routes/index.ts (QA human review #1)
    Registro da rota de login.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,3 @@
-import { Router } from 'express';
-import { UserController } from '../controllers/UserController';
+fix: corrige src/routes/index.ts (QA human review #1)
 
-const router = Router();
-router.post('/auth/login', UserController.login);
-export default router;
+Registro da rota de login.
