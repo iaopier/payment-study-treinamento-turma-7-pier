66598c8d73a93cff5d4f045cf653d1df393bd15d
Piercarlo Fermino Soares <pierhett@gmail.com>
2026-07-20 14:46:59 -0300

    fix: corrige src/controllers/UserController.ts (QA human review #2)
    Implementação dos métodos register e login com tratamento de erros específico.

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,3 +1,3 @@
-fix: corrige src/controllers/UserController.ts (QA human review #1)
+fix: corrige src/controllers/UserController.ts (QA human review #2)
 
 Implementação dos métodos register e login com tratamento de erros específico.
