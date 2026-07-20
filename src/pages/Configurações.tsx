6883ac2bbc2cf2b967a666b41b5139123f20aa99
Piercarlo Fermino Soares <pierhett@gmail.com>
2026-07-20 14:13:00 -0300

    feat(pages): [KAN-6] Ajuste na estrutura do formulário para garantir que o LoginB

---
+++ src/pages/Configurações.tsx
@@ -0,0 +1,1 @@
+import React from 'react'; import { LoginButton } from '../components/LoginButton'; export const Configuracoes: React.FC = () => { return (<div className="config-container"><h1>Configurações</h1><form className="config-form"><div className="form-group"><label>Email</label><input type="email" /></div><LoginButton /> </form></div>); };
