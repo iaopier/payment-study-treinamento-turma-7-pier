6232914d9400fd04a4d05117f862d3f250d44f6d
Piercarlo Fermino Soares <pierhett@gmail.com>
2026-07-20 13:59:14 -0300

    fix: corrige src/middlewares/authMiddleware.ts (QA human review #1)
    Implementação do middleware de verificação de token JWT.

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,3 +1,3 @@
 fix: corrige src/middlewares/authMiddleware.ts (QA human review #1)
 
-Criação do middleware de autenticação para validar o token JWT.
+Implementação do middleware de verificação de token JWT.
