219600f9c089020881e63ce56b83b1e10df4047a
Piercarlo Fermino Soares <pierhett@gmail.com>
2026-07-21 15:18:03 -0300

    fix: corrige src/routes/notification.routes.ts (QA human review #2)
    Definição das rotas de notificação com middleware de autenticação.

--- src/routes/notification.routes.ts
+++ src/routes/notification.routes.ts
@@ -1,3 +1,3 @@
-fix: corrige src/routes/notification.routes.ts (QA human review #1)
+fix: corrige src/routes/notification.routes.ts (QA human review #2)
 
 Definição das rotas de notificação com middleware de autenticação.
