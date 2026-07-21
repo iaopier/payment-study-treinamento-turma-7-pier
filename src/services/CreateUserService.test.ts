00204fbe0b8a8a1fd1d02a0703f77958616275a9
Piercarlo Fermino Soares <pierhett@gmail.com>
2026-07-21 14:55:11 -0300

    fix: corrige src/services/CreateUserService.test.ts (QA human review #1)
    Testes para o serviço de criação de usuário.

--- src/services/CreateUserService.test.ts
+++ src/services/CreateUserService.test.ts
@@ -1,3 +1,3 @@
-fix: corrige src/services/CreateUserService.test.ts (QA human review #2)
+fix: corrige src/services/CreateUserService.test.ts (QA human review #1)
 
-Testes unitários para o serviço de criação de usuário.
+Testes para o serviço de criação de usuário.
