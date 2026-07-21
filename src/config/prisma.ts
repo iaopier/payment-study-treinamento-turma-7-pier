02fe8e78b679eb2135f3288223e3d902beb5c8ce
Piercarlo Fermino Soares <pierhett@gmail.com>
2026-07-21 14:31:26 -0300

    fix: corrige src/config/prisma.ts (QA human review #1)
    Configuração do PrismaClient com suporte a auto-generate.

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,1 +1,3 @@
-import { PrismaClient } from '@prisma/client'; export const prisma = new PrismaClient();
+fix: corrige src/config/prisma.ts (QA human review #1)
+
+Configuração do PrismaClient com suporte a auto-generate.
