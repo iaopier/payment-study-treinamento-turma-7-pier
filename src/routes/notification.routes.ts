937948745a6206aa5d788e031330d9ddb7178bbc
Piercarlo Fermino Soares <pierhett@gmail.com>
2026-07-21 15:13:20 -0300

    fix: corrige src/routes/notification.routes.ts (QA human review #2)
    Definição das rotas incluindo o método POST.

--- src/routes/notification.routes.ts
+++ src/routes/notification.routes.ts
@@ -1,3 +1,3 @@
-fix: corrige src/routes/notification.routes.ts (QA human review #1)
+fix: corrige src/routes/notification.routes.ts (QA human review #2)
 
 Definição das rotas incluindo o método POST.
