26dca21363dda9505277bf819c0922c55ef49a37
Piercarlo Fermino Soares <pierhett@gmail.com>
2026-07-21 15:39:31 -0300

    fix: corrige src/controllers/UserController.ts (QA human review #2)
    Adição dos endpoints de listagem e marcação de notificações com tratamento de erros.

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,3 +1,3 @@
-fix: corrige src/controllers/UserController.ts (QA human review #1)
+fix: corrige src/controllers/UserController.ts (QA human review #2)
 
-Adição de endpoints para listagem e marcação de notificações com tratamento de erros.
+Adição dos endpoints de listagem e marcação de notificações com tratamento de erros.
